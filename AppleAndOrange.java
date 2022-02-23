8dba2f5aca5b8a5ad09cce37b4e3ab971175fbe2
Nathiya12 <100260155+Nathiya12@users.noreply.github.com>
2022-02-23 11:39:35 +0530

    Add files via upload

---
+++ AppleAndOrange.java
@@ -0,0 +1,35 @@
+package algorithamprograms;
+
+import java.util.Scanner;
+
+public class AppleAndOrange {
+
+public static void main(String[]args) {	
+	
+	 Scanner in = new Scanner(System.in);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
